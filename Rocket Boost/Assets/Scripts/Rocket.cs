d5a96d4a78c3bacacc2792943282b2592a8a0e5d
ValentinGurkov <valentingurkov@gmail.com>
2019-07-08 21:36:55 +0300

    Rocket thrust audio and organised files into respective folders

--- Rocket Boost/Assets/Scripts/Rocket.cs
+++ Rocket Boost/Assets/Scripts/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
     private Rigidbody rigidBody;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     private void Start() {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -19,6 +21,11 @@ public class Rocket : MonoBehaviour {
     private void ProcessInput() {
         if (Input.GetKey(KeyCode.Space)) {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+        } else {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(Vector3.forward);
