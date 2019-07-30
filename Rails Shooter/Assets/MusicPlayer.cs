f116f8346dc0f15e7a120c6959c0a097aa23411b
ValentinGurkov <valentingurkov@gmail.com>
2019-07-30 22:47:45 +0300

    loading screen

---
+++ Rails Shooter/Assets/MusicPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    // Start is called before the first frame update
+    private void Start() {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    // Update is called once per frame
+    private void LoadFirstScene() {
+        SceneManager.LoadScene(1);
+    }
+}
