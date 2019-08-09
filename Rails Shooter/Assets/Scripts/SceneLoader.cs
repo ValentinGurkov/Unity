01e12a40ee39c078e1e4e2db3302e2a9cb442814
ValentinGurkov <valentingurkov@gmail.com>
2019-08-09 12:57:18 +0300

    Did some spring cleaing

--- Rails Shooter/Assets/Scripts/SceneLoader.cs
+++ Rails Shooter/Assets/Scripts/SceneLoader.cs
@@ -1,13 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour {
-
-    private void Awake() {
-        DontDestroyOnLoad(gameObject);
-    }
+public class SceneLoader : MonoBehaviour {
 
     // Start is called before the first frame update
     private void Start() {
