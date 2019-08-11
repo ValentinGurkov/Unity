b9f3e6aa70af03266288e0f69788b294b1f1372c
ValentinGurkov <valentingurkov@gmail.com>
2019-08-11 16:08:40 +0300

    Cube with label coords on top

---
+++ Tower Defence/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
+    [SerializeField] [Range(1f, 20f)] private float gridSize = 10f;
+
+    private TextMesh textMesh;
+
+    private void Update() {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = 0f;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        textMesh = gameObject.GetComponentInChildren<TextMesh>();
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+
+        transform.position = snapPos;
+    }
+}
